066f19c498d6db9ac55f23ef4ed051fdda575b0b
Owei88 <darthreven78@gmail.com>
2021-11-11 17:33:58 -0700

    Attempting commit

--- SpaRewards.java
+++ SpaRewards.java
@@ -2,7 +2,7 @@ public class SpaRewards {
     public static int numOfManicures;
     public static int numOfReferrals;
     public int discountPercent;
-//a
+
     public int calculateDiscount(int numOfManicures, int numOfReferrals){
         discountPercent = numOfManicures + numOfReferrals;
         if (discountPercent > 75){
