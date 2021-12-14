fcc1a0f9208117b6fb8bc1b9cad2082d2b8b0892
Owei88 <darthreven78@gmail.com>
2021-12-13 17:13:25 -0700

    Edited Code

--- SpaTester.java
+++ SpaTester.java
@@ -11,7 +11,11 @@ public class SpaTester {
         int numberOfManicures = in.nextInt();
         System.out.print("Enter number of referrals: ");
         int numberOfReferrals = in.nextInt();
-        System.out.printf("The discount is equal to: "+ reward.calculateDiscount(numberOfManicures,numberOfReferrals)+".00");
+        System.out.printf("The discount is equal to: "+ reward.calculateDiscount(numberOfManicures,numberOfReferrals));
+        System.out.println("The discount should be the number of manicures plus referrals, " +
+                "unless it exceeds 75, in which case it will be 75");
+        System.out.println("For example, 58 manicures and 4 referrals would be a discount of 62, but " +
+                "78 manicure and 6 referrals would be a discount of 75");
 
 
     }
